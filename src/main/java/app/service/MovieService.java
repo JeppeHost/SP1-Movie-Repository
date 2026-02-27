26ed886c4e5fb0b27ff25c48112b037a97a434e5
Tesselation93 <tessvanslyckspam@gmail.com>
2026-02-27 13:44:43 +0100

    Update MovieService.java

--- src/main/java/app/service/MovieService.java
+++ src/main/java/app/service/MovieService.java
@@ -5,6 +5,8 @@ import app.daos.ActorDAO;
 import app.daos.DirectorDAO;
 import app.daos.GenreDAO;
 import app.daos.MovieDAO;
+import app.dtos.CreditsDTO;
+import app.dtos.GenreDTO;
 import app.dtos.MovieDTO;
 import app.entities.Actor;
 import app.entities.Director;
@@ -18,6 +20,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,19 +38,18 @@ public class MovieService {
     private final GenreDAO genreDAO = new GenreDAO(emf);
     private final DirectorDAO directorDAO = new DirectorDAO(emf);
 
-    public MovieDTO getMovieById(Long id) throws Exception {
-        String json = fetch(BASE_URL + "/movie/" + id + "?api_key=" + apiKey);
-        return objectMapper.readValue(json, MovieDTO.class);
-    }
-
     public List<MovieDTO> getDanishMovies() throws Exception {
         List<MovieDTO> all = new ArrayList<>();
         int page = 1;
+        String fiveYearsAgo = LocalDate.now().minusYears(5).toString();
 
         while (true) {
             String json = fetch(BASE_URL + "/discover/movie?api_key=" + apiKey
-                    + "&with_original_language=da&page=" + page);
+                    + "&with_original_language=da"
+                    + "&primary_release_date.gte=" + fiveYearsAgo
+                    + "&page=" + page);
             MovieDTO.PageResult response = objectMapper.readValue(json, MovieDTO.PageResult.class);
+            if (response.getResults() == null) break;
             all.addAll(response.getResults());
             if (page >= response.getTotalPages()) break;
             page++;
@@ -55,46 +57,72 @@ public class MovieService {
         return all;
     }
 
+    public CreditsDTO getCredits(Long movieId) throws Exception {
+        String json = fetch(BASE_URL + "/movie/" + movieId + "/credits?api_key=" + apiKey);
+        return objectMapper.readValue(json, CreditsDTO.class);
+    }
+
     public void fetchAndSaveAllDanishMovies() throws Exception {
         List<MovieDTO> movies = getDanishMovies();
         for (MovieDTO dto : movies) {
-            Movie movie = toEntity(dto);
+            CreditsDTO credits = getCredits(dto.getId());
+            Movie movie = toEntity(dto, credits);
+
+            if (movie.getDirector() != null && movie.getDirector().getId() == null) {
+                directorDAO.save(movie.getDirector());
+            }
+
+            for (Actor actor : movie.getActors()) {
+                if (actor.getId() == null) {
+                    actorDAO.save(actor);
+                }
+            }
+
+            for (Genre genre : movie.getGenres()) {
+                if (genre.getId() == null) {
+                    genreDAO.save(genre);
+                }
+            }
+
             movieDAO.save(movie);
         }
     }
 
-    public Movie toEntity(MovieDTO dto) {
+    public Movie toEntity(MovieDTO dto, CreditsDTO credits) {
         Movie movie = new Movie();
+        movie.setId(dto.getId().intValue());
         movie.setTitle(dto.getTitle());
         movie.setOverview(dto.getOverview());
-        movie.setVoteAverage(dto.getVoteAverage());
+        movie.setRating(dto.getVoteAverage());
         movie.setReleaseDate(dto.getReleaseDate());
         movie.setOriginalLanguage(dto.getOriginalLanguage());
+        movie.setPopularity(dto.getPopularity());
 
         if (dto.getGenres() != null) {
-            List<Genre> genres = dto.getGenres().stream()
-                    .map(g -> {
-                        Genre genre = new Genre();
-                        genre.setName(g.getName());
-                        return genre;
-                    }).toList();
-            movie.setGenres(genres);
+            for (GenreDTO genreDTO : dto.getGenres()) {
+                Genre genre = genreDAO.findByName(genreDTO.getName());
+                if (genre == null) genre = new Genre(genreDTO.getName());
+                movie.addGenre(genre);
+            }
         }
 
-        if (dto.getCast() != null) {
-            List<Actor> actors = dto.getCast().stream()
-                    .map(a -> {
-                        Actor actor = new Actor();
-                        actor.setName(a.getName());
-                        return actor;
-                    }).toList();
-            movie.setActors(actors);
+        if (credits.getCast() != null) {
+            for (CreditsDTO.CastMemberDTO castMember : credits.getCast()) {
+                Actor actor = actorDAO.findByName(castMember.getName());
+                if (actor == null) actor = new Actor(castMember.getName());
+                movie.addActor(actor);
+            }
         }
 
-        if (dto.getDirector() != null) {
-            Director director = new Director();
-            director.setName(dto.getDirector().getName());
-            movie.setDirector(director);
+        if (credits.getCrew() != null) {
+            credits.getCrew().stream()
+                    .filter(c -> "Director".equals(c.getJob()))
+                    .findFirst()
+                    .ifPresent(directorMember -> {
+                        Director director = directorDAO.findByName(directorMember.getName());
+                        if (director == null) director = new Director(directorMember.getName());
+                        movie.setDirector(director);
+                    });
         }
 
         return movie;
